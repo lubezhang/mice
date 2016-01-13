be92a44e029a6993706baa6882f8d11e34e7f619
zhangqinghong <zhangqinghong@360.cn>
2016-01-13 11:29:19 +0800

    博客页与后台管理的react公共模块，博客页以后不用react实现

--- www/src/view/admin/react/components/Table.jsx
+++ www/src/view/admin/react/components/Table.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Pagination } from 'react-bootstrap';
-var _ = require("underscore");
+var _ = require("lodash");
 
 class Table extends Component {
 
