28c498d67534f1cc01ac783eb27c514cd6bc558e
zhangqinghong <zhangqinghong@360.cn>
2016-01-14 18:01:11 +0800

    增加开发环境的对外服务端口和开发数据库的配置

--- src/common/config/env/development.js
+++ src/common/config/env/development.js
@@ -1,5 +1,27 @@
 'use strict';
 
 export default {
-  
+  port: 8360,
+  log_request: true,
+  db: {
+    type: 'mysql',
+      host: '10.16.93.35',
+      port: '3306',
+      name: 'blog',
+      user: 'root',
+      pwd: 'qwe123',
+      prefix: '',
+      encoding: 'utf8',
+      nums_per_page: 10,
+      log_sql: true,
+      log_connect: true,
+      cache: {
+        on: true,
+        type: '',
+        timeout: 3600
+      },
+      options: {
+        authSource: 'admin'
+      }
+  }
 };
